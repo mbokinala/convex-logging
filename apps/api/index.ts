d8d06a8df89411e44fed71bbc6069a99485f644b
Manav Bokinala <manav.bokinala@gmail.com>
2025-11-29 15:10:54 -0800

    Add timestamp skew and HMAC signature checks to ingest server

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -29,6 +29,57 @@ app.post("/ingest", async (c) => {
   const split = data.split("\n");
   const events = split.map((e) => JSON.parse(e));
 
+  /**
+   * Validate that the timestamp of the first log is within 5 minutes.
+   * This helps prevent replay attacks
+   */
+
+  const maxAllowedTimestampSkew = parseInt(
+    process.env.MAX_ALLOWED_TIMESTAMP_SKEW || "0"
+  );
+
+  if (maxAllowedTimestampSkew > 0) {
+    const firstEvent = events[0];
+    if (firstEvent.timestamp < Date.now() - maxAllowedTimestampSkew * 1000) {
+      c.status(403);
+      return c.text("Request expired");
+    }
+  }
+
+  // Validate the webhook signature
+  if (process.env.WEBHOOK_SECRET) {
+    const signature = c.req.header("x-webhook-signature");
+    if (!signature) {
+      c.status(401);
+      return c.text("Unauthorized");
+    }
+
+    const hmacSecret = await crypto.subtle.importKey(
+      "raw",
+      new TextEncoder().encode(process.env.WEBHOOK_SECRET),
+      {
+        name: "HMAC",
+        hash: "SHA-256",
+      },
+      false,
+      ["verify"]
+    );
+    const hashPayload = await c.req.arrayBuffer();
+
+    const isValid = await crypto.subtle.verify(
+      "HMAC",
+      hmacSecret,
+      // @ts-expect-error - this is valid in V8 now
+      Uint8Array.fromHex(signature.replace("sha256=", "")),
+      hashPayload
+    );
+
+    if (!isValid) {
+      c.status(401);
+      return c.text("Unauthorized");
+    }
+  }
+
   const functionExecutionEvents = events.filter(
     (e) => e.topic === "function_execution"
   );
