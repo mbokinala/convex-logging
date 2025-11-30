8f8e902689e5c9e484ebe0d08b9e1a8b717d31da
Manav Bokinala <manav.bokinala@gmail.com>
2025-11-30 14:34:01 -0800

    Add console_log table to setup.sql

--- apps/ingest-server/setup.sql
+++ apps/ingest-server/setup.sql
@@ -1,3 +1,6 @@
+SET allow_experimental_full_text_index = true;
+
+
 CREATE TABLE IF NOT EXISTS default.function_execution
 (
     `function_type` String,
@@ -15,3 +18,23 @@ CREATE TABLE IF NOT EXISTS default.function_execution
 ENGINE = MergeTree
 ORDER BY (function_path, timestamp, status)
 SETTINGS index_granularity = 8192
+
+CREATE TABLE IF NOT EXISTS default.console_log
+(
+    `function_type` String,
+    `function_path` String,
+    function_cached Bool DEFAULT 'FALSE',
+    `request_id` String,
+    `timestamp` DateTime,
+    `log_level` String,
+    `message` String,
+    `is_truncated` Bool DEFAULT 'FALSE',
+    `system_code` String,
+
+    INDEX idx_message(message) TYPE text(
+        tokenizer = splitByNonAlpha
+    ) GRANULARITY 64
+)
+ENGINE = MergeTree
+ORDER BY (function_path, timestamp, log_level)
+SETTINGS allow_experimental_full_text_index = 1;
